0e4e051412caa31d853758e626653801cd7fd2a1
shohag390 <shohag390@users.noreply.github.com>
2025-04-19 22:02:30 +0600

    Pricing section add

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
+import PricingOption from './Components/PricingOption/PricingOption';
 
 
 const App = () => {
+
+  const fetchData = fetch("pricingData.json").then((res) => res.json());
+
+
+
   return (
     <>
       <header>
         <Navbar />
       </header>
       <main>
-
+        <Suspense fallback={<h1>Loading...</h1>}>
+          <PricingOption fetchData={fetchData} />
+        </Suspense>
       </main>
       <footer>
         <Footer />
---
+++ src/Components/PricingOption/PricingOption.jsx
@@ -0,0 +1,33 @@
+import React, { use } from 'react';
+
+const PricingOption = ({ fetchData }) => {
+    const pricingData = use(fetchData);
+    console.log(pricingData);
+
+
+    return (
+        <div className='md:py-[40px] py-[20px] grid md:grid-cols-3 grid-cols-1 gap-[40px] container mx-auto md:px-[80px] px-[20px]'>
+            {pricingData?.map((item) => (
+                <div style={{
+                    borderColor: `${item?.color}`
+                }} className='w-full flex flex-col text-center items-center bg-[#06013d] text-[#fff] md:p-[30px] p-[20px] rounded-2xl border-b-[8px]' key={item?.id}>
+                    <h4 className='font-bold text-[20px] uppercase leading-[30px] pb-[20px]'>{item?.name}</h4>
+                    <div className='flex flex-col gap-[8px] pb-[20px]'>
+                        <p>{item?.features[0]}</p>
+                        <p>{item?.features[1]}</p>
+                        <p>{item?.features[2]}</p>
+                        <p>{item?.features[3]}</p>
+                    </div>
+                    <h2 className='text-[22px] font-medium'>{item?.price}</h2>
+                    <button className='py-[8px] px-[30px] rounded-md mt-[20px]' style={{
+                        background: `${item?.color}`
+                    }}>
+                        BUY NOW
+                    </button>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default PricingOption;
