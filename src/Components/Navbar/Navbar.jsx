5c4e4f75bf804936c0ff2374c02294bf973f54f3
shohag390 <shohag390@users.noreply.github.com>
2025-04-19 19:13:08 +0600

    Navbar Ok

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -43,20 +43,20 @@ const Navbar = () => {
     }
 
     return (
-        <nav className='flex items-center justify-between container mx-auto md:px-[80px] px-[20px] md:h-[10vh] h-[8vh] z-50 bg-[#06013d]'>
-            <a className='font-bold text-[25px] text-[#5c5c5c]' href="/">
+        <nav className='flex items-center justify-between container mx-auto md:px-[80px] px-[20px] md:h-[10vh] h-[8vh] sticky top-0 left-0 z-50 bg-[#06013d]'>
+            <a className='font-bold md:text-[25px] text-[22px] text-[#fff]' href="/">
                 LOGO
             </a>
             <ul className='md:flex md:items-center md:gap-[30px] uppercase hidden'>
                 {
-                    navlink.map((link) => <li key={link.id} className='font-medium text-[#5c5c5c]'><a href={link.path}>{link.name}</a></li>)
+                    navlink.map((link) => <li key={link.id} className='font-medium text-[#ffffff]'><a href={link.path}>{link.name}</a></li>)
                 }
             </ul>
-            <div>
-                <button onClick={handleOpen} className='md:hidden'>
+            <div className='md:hidden'>
+                <button onClick={handleOpen}>
                     {!open ? <IoMenu className='text-[25px] text-[#fff]' /> : <IoClose className='text-[28px] text-[#fff]' />}
                 </button>
-                <ul className={`uppercase md:hidden duration-1000 w-full left-0 flex items-center flex-col gap-[20px] py-[50px] ${open ? "top-[8vh]" : "-top-[580px]"} absolute w-full bg-[#06013d] z-0`}>
+                <ul className={`uppercase md:hidden duration-700 w-full top-[8vh] flex items-center flex-col gap-[20px] py-[50px] ${open ? "left-0" : "-left-[100%]"} absolute w-full bg-[#06013d] -z-50`}>
                     {
                         navlink.map((link) => <li key={link.id} className='font-medium text-[#ffffff]'><a href={link.path}>{link.name}</a></li>)
                     }
